c6b34573ac05ad9d3ceba1a6e208a26920f413a0
wyd <pk209654321@163.com>
2018-07-23 11:13:02 +0800

    20180723

--- src/test/scala/cn/itcat/TestHash.scala
+++ src/test/scala/cn/itcat/TestHash.scala
@@ -6,16 +6,7 @@ package cn.itcat
 object TestHash {
 
   def main(args: Array[String]) {
-    val i : Long = 1
-    val j : Long = 2
-    println(i / j)
-    //    val key = "php.itcast.cn"
-    //    val mod = 5
-    //    val rawMod = key.hashCode % mod
-    //    val partNum = rawMod + (if (rawMod < 0) mod else 0)
-    //    println(partNum)
-    //val _type = 1
-    //println(FilterUtils.filterByTypes(Array("1", "2"), "4", "2", "3"))
+    println("a")
   }
 
 }
